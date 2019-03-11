87ddfa24122e1cc70f82607cedc979261e062d36
Daria Buksha <d.buksha@wis.eu.com>
2019-03-11 11:01:31 +0300

    get user data from JWT

--- app/javascript/application/store/modules/auth.js
+++ app/javascript/application/store/modules/auth.js
@@ -1,4 +1,9 @@
+import jwtDecode from 'jwt-decode';
+
 import instance from '../instance';
+import {
+  mutationTypes as usersMutations,
+} from './users';
 
 const endpoints = {
   CREATE: '/sign_in',
@@ -15,6 +20,13 @@ export const mutationTypes = {
   CLEAR_JWT: 'CLEAR_JWT',
 };
 
+function decodeJWT(token) {
+  let payload = jwtDecode(token);
+  delete payload.exp;
+
+  return payload;
+}
+
 export default {
   namespaced: true,
 
@@ -28,6 +40,8 @@ export default {
         .then(({ data }) => {
           commit(mutationTypes.SET_JWT, data.token);
           commit(mutationTypes.SET_IS_LOGGED_IN, true);
+          commit(`users/${usersMutations.SET_USER}`, { user: decodeJWT(data.token) }, { root: true });
+
           return Promise.resolve();
         });
     },
